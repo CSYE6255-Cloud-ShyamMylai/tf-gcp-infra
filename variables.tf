8ab9ebac6ba734a69c364d2ca21b72e6f46537e8
Shyam Sundar Mylai <mylai.s@northeastern.edu>
2024-02-15 14:33:48 -0500

    Terraform Major Updates
    - Added For each to loop through vpc
    - Change variable structures in variables.tf to map data strucutre

--- variables.tf
+++ variables.tf
@@ -5,59 +5,25 @@ variable "gcp_credentials_file" {
 variable "projectid" {
   description = "projectId for the project"
 }
-
-variable "vpc_network_name" {
-  description = "name of the vpc network"
+variable "vpcs" {
+  description = "List  of VPC Maps"
+  type = map(object({
+    name                                        = string
+    subnet_1                                    = string
+    subnet_2                                    = string
+    subnet_region                               = string
+    subnet_1_cidr_range                         = string
+    subnet_2_cidr_range                         = string
+    auto_create_subnetworks_flag                = bool
+    routing_mode_vpc_network                    = string
+    delete_default_routes_on_create_vpc_network = bool
+    stack_type_vpc_network                      = string
+    vpc_route_name                              = string
+    vpc_route_dest_address                      = string
+    default_internet_gateway                    = string
+  }))
 }
 
-variable "subnet_1" {
-  description = "name of the first subnet (webapp)"
-}
-
-variable "subnet_2" {
-  description = "name of the second subnet (db)"
-}
-
-variable "subnet_region" {
-  description = "region of the subnet"
-}
-
-variable "subnet_1_cidr_range" {
-  description = "cidr range for subnet1"
-}
-
-variable "subnet_2_cidr_range" {
-  description = "cidr range for subnet2"
-}
-
-variable "auto_create_subnetworks_flag" {
-  description = "subnetworks flag"
-}
-
-variable "routing_mode_vpc_network" {
-  description = "region for vpc network"
-}
-
-variable "delete_default_routes_on_create_vpc_network" {
-  description = "delete default routes on vpc network"
-}
-
-variable "stack_type_vpc_network" {
-  description = "stack type for vpc network"
-}
-
-variable "secondary_ip_range_subnets" {
-  description = "secondary ip range for subnet"
-}
-
-variable "vpc_route_name" {
-  description = "name of the route"
-}
-
-variable "vpc_route_dest_address" {
-  description = "destination address for the route"
-}
-
-variable "default_internet_gateway" {
-  description = "default internet gateway"
+variable "projectregion" {
+  description = "region for the project"
 }
